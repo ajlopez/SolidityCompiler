6f12fdf7b7d349ca9d5198b0a3f4f8febdd5d836
ajlopez <webmaster@ajlopez.com>
2016-08-10 15:34:05 -0300

    get two name tokens

--- test/lexer.js
+++ test/lexer.js
@@ -33,6 +33,24 @@ exports['get name token without spaces'] = function (test) {
 	test.equal(lexer.nextToken(), null);
 }
 
+exports['get two name tokens'] = function (test) {
+	var lexer = lexers.lexer('foo bar');
+	
+	var token = lexer.nextToken();
+	
+	test.ok(token);
+	test.equal(token.value, 'foo');
+	test.equal(token.type, TokenType.Name);
+	
+	var token = lexer.nextToken();
+	
+	test.ok(token);
+	test.equal(token.value, 'bar');
+	test.equal(token.type, TokenType.Name);
+	
+	test.equal(lexer.nextToken(), null);
+}
+
 
 
 
