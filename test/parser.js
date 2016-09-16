f58c64ae93bb7bb9588e9ad97e8828140bae83e3
ajlopez <webmaster@ajlopez.com>
2016-09-16 13:45:22 -0300

    parse module integers

--- test/parser.js
+++ test/parser.js
@@ -98,6 +98,16 @@ exports['parse divide integers'] = function (test) {
 	test.equal(expr.right().value(), 3);
 };
 
+exports['parse module integers'] = function (test) {
+	var parser = parsers.parser('2 % 3');
+	
+	var expr = parser.parseExpression();
+	
+	test.ok(expr);
+	test.equal(expr.operator(), '%');
+	test.equal(expr.left().value(), 2);
+	test.equal(expr.right().value(), 3);
+};
 exports['parse add and divide integers'] = function (test) {
 	var parser = parsers.parser('1+2/3');
 	
