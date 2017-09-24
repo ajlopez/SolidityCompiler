8e343587b3ab299530b7882747c2451015057bae
ajlopez <webmaster@ajlopez.com>
2017-09-24 18:08:12 -0300

    Compile empty contract to Java

--- test/compilers/java.js
+++ test/compilers/java.js
@@ -226,3 +226,12 @@ exports['compile function with two arguments, int type and non-empty body'] = fu
     test.deepEqual(result, [ 'public Int256 fn(Int256 a, Int256 b) {', '    return a.add(b);', '}' ]);
 }
 
+exports['compile empty contract'] = function (test) {
+    var result = compileCommand('contract Empty { }');
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+
+    test.deepEqual(result, [ 'public class Empty extends Contract {', '}' ]);
+}
+
